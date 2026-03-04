509dd3689b832d7222c6dffbed0e50937cb126f3
MOHAMMED RASHEED <mohammedrasheed9609@gmail.com>
2026-03-04 07:32:28 +0530

    Update 02_kpi_summary.sql

--- Sql_Scripts/02_kpi_summary.sql
+++ Sql_Scripts/02_kpi_summary.sql
@@ -1,1 +1,24 @@
+-- ============================================================================================
+-- KPI SUMMARY
+-- MONTHLY REVENUE, PROFIT AND MARGIN ANALYSIS
+-- ============================================================================================
 
+-- Management wants to see current month performance summary
+;with current_month as (
+select 
+	count(*) as total_orders,
+	sum(amount) as total_revenue,
+	sum(cost) as total_cost
+from sales
+where sale_date >='2023-01-01' and sale_date < '2023-02-01'
+)
+select 
+	total_orders,
+	Round(total_revenue,2) as total_revenue,
+	ROUND(total_cost,2) as total_cost,
+	Round(total_revenue - total_cost,2) as gross_profit,
+	concat(ROUND(
+		(total_revenue - total_cost) * 100.0 / nullif(total_revenue, 0),
+		2
+	),'%') as Gross_Margin_Percentage
+from current_month
