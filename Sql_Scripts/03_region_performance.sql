5d536f6cfd65eaff7dae915ae9c0459064932fb3
MOHAMMED RASHEED <mohammedrasheed9609@gmail.com>
2026-03-04 07:35:10 +0530

    Create 03_region_performance.sql

---
+++ Sql_Scripts/03_region_performance.sql
@@ -0,0 +1,29 @@
+-- ============================================================================================
+--
+--
+-- ============================================================================================
+
+-- Region wise monthly performance
+
+;with region_performance as (
+
+select 
+	region,
+	count(*) as total_orders,
+	sum(amount) as total_revenue,
+	sum(cost) as total_cost,
+	ROUND(sum(amount) - sum(cost),2) as gross_profit,
+	CONCAT(ROUND( 
+		(sum(amount) - sum(cost)) * 100.0 
+		/ nullif(sum(amount), 0), 
+		2
+	),' %') as gross_margin_percentage,
+	count(distinct customer_id) as unique_customers
+from sales
+where sale_date >= '2023-01-01' and sale_date < '2023-02-01'
+group by region
+
+)
+select * from region_performance
+order by total_revenue desc
+
